2e048b0d510e23e9e161bdf94590ed6fbc90662e
Miaojj <yijunmiao287@gmail.com>
2026-01-24 06:07:08 +0800

    update

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./index.tsx",
+    "./App.tsx",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        handwriting: ['Kalam', 'cursive'],
+        marker: ['Permanent Marker', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
